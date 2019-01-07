42b40c8131533afa589586f3c2c0b71cec9c497b
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-01-07 13:57:36 +0900

    feat: add Constant Class

---
+++ app/src/main/java/ensharp/yeey/whisperer/Constant.java
@@ -0,0 +1,11 @@
+package ensharp.yeey.whisperer;
+
+//  상수 클래스
+public class Constant {
+
+    //어린이대공원역(세종대역) 위도 경도 좌표
+    public static final double Sejong_University_latitude = 37.547736;
+    public static final double Sejong_University_longitude = 127.074437;
+
+    
+}
