1bd6e121fb1257bc83d602628c9aad350b9c5613
Yejining <kimyejin.kr@gmail.com>
2019-01-09 18:16:56 +0900

    Setting: Set gradle to use API for getting subway running informations

--- app/src/main/java/ensharp/yeey/whisperer/MainActivity.java
+++ app/src/main/java/ensharp/yeey/whisperer/MainActivity.java
@@ -2,12 +2,36 @@ package ensharp.yeey.whisperer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.odsay.odsayandroidsdk.API;
+import com.odsay.odsayandroidsdk.ODsayData;
+import com.odsay.odsayandroidsdk.ODsayService;
+import com.odsay.odsayandroidsdk.OnResultCallbackListener;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ODsayService odsayService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initSubwayAPI();
+    }
+
+    /**
+     * 지하철 운행정보를 가져오는 API를 호출합니다.
+     * ODsayService 객체는 싱글톤으로 생성됩니다.
+     */
+    private void initSubwayAPI() {
+        odsayService = ODsayService.init(MainActivity.this, getString(R.string.odsay_key));
+        odsayService.setReadTimeout(5000);
+        odsayService.setConnectionTimeout(5000);
     }
 }
